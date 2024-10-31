3d8e0a788cb0cdb66a51bc7b43082b23cf06dd7c
Jonas Birmé <jonas.birme@eyevinn.se>
2024-10-31 13:49:44 +0100

    fix: only download if software not installed

--- osc-entrypoint.sh
+++ osc-entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-unzip /source/suitecrm-8-7-0.zip -d /var/www/html
+if [ ! -f /var/www/html/VERSION ]; then
+  echo "SuiteCRM not found in /var/www/html - downloading software now..."
+  unzip /source/suitecrm-8-7-0.zip -d /var/www/html
 
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
-find /var/www/html -type d -not -perm 2755 -exec chmod 2755 {} \;
-find /var/www/html -type f -not -perm 0644 -exec chmod 0644 {} \;
-find /var/www/html ! -user www-data -exec chown www-data:www-data {} \;
-chmod +x /var/www/html/bin/console
+  chown -R www-data:www-data /var/www/html
+  chmod -R 755 /var/www/html
+  find /var/www/html -type d -not -perm 2755 -exec chmod 2755 {} \;
+  find /var/www/html -type f -not -perm 0644 -exec chmod 0644 {} \;
+  find /var/www/html ! -user www-data -exec chown www-data:www-data {} \;
+  chmod +x /var/www/html/bin/console
+fi
 
 docker-php-entrypoint "$@"
