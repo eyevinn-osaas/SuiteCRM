8da218a7743b9f430447de02f97456557397e402
Jonas Birmé <jonas.birme@eyevinn.se>
2025-10-15 17:10:17 +0200

    fix: osc required patch

--- osc-entrypoint.sh
+++ osc-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /var/www/html/VERSION ]; then
   echo "SuiteCRM not found in /var/www/html - downloading software now..."
-  unzip /source/suitecrm-8-7-0.zip -d /var/www/html
+  unzip /source/suitecrm-8-9-0.zip -d /var/www/html
 
   chown -R www-data:www-data /var/www/html
   chmod -R 755 /var/www/html
@@ -10,6 +10,7 @@ if [ ! -f /var/www/html/VERSION ]; then
   find /var/www/html -type f -not -perm 0644 -exec chmod 0644 {} \;
   find /var/www/html ! -user www-data -exec chown www-data:www-data {} \;
   chmod +x /var/www/html/bin/console
+  cd /var/www/html/core/backend/Install/Service/ && patch -p1 InstallPreChecks.php < /source/osc.patch
 fi
 
 docker-php-entrypoint "$@"
